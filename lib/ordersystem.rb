44abdce41dd49c76ea103cdf070fa0998dbdc2f0
francescoFH <francescofacca@gmail.com>
2021-08-05 00:47:47 +0100

    Added failing tests checking the order shows the correct summary and the correct total

--- lib/ordersystem.rb
+++ lib/ordersystem.rb
@@ -21,8 +21,7 @@ class OrderSystem
 
     fail "This isn't on the order" if in_order? dish
 
-    order.delete_if { |has| hash[:dish] == dish }
-
+    order.delete_if { |hash| hash[:dish] == dish }
   end
 
   def view_order
