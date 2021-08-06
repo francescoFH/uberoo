b8b031b61685d7b55c9023c767bf7a9d4804a81d
francescoFH <francescofacca@gmail.com>
2021-08-06 16:47:09 +0100

    Added method checkout to confirm order and get confirmation text and done some refactoring

--- lib/ordersystem.rb
+++ lib/ordersystem.rb
@@ -1,4 +1,5 @@
 require_relative 'takeaway_kitchen'
+require_relative 'text'
 
 class OrderSystem
   attr_reader :menu, :order
@@ -14,7 +15,7 @@ class OrderSystem
 
   def add_to_order(dish, quantity=1)
     fail "This isn't on the menu" if in_menu? dish
-    
+
     order << { dish: dish, quantity: quantity, price: calculate_price(dish, quantity) }
   end
 
@@ -31,11 +32,12 @@ class OrderSystem
   end
 
   def view_total
-    "£#{order.map { |item| item[:price] }.compact.reduce(:+)}"
+    puts "Order total is: £#{order.map { |item| item[:price] }.compact.reduce(:+)}"
   end
 
-  def place_order
-
+  def checkout
+    puts "Happy to proceed with order (y/n) ?"
+    gets.chomp == "y" ? Text.new.send_text : return
   end
 
   private
--- spec/takeaway_kitchen_spec.rb
+++ spec/takeaway_kitchen_spec.rb
@@ -1,9 +1,7 @@
 require 'takeaway_kitchen'
 
 describe TakeawayKitchen do
-
-  it { is_expected.to respond_to :return_menu }
-
+  
   it 'has a menu' do
     expect(subject.menu).not_to be_empty
   end
