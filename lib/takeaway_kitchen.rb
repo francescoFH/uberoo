d6cc7bdf07c40ff2377cb79b0de2d293ea61232f
francescoFH <francescofacca@gmail.com>
2021-08-06 16:53:24 +0100

    Updated menu with real dishes

--- lib/takeaway_kitchen.rb
+++ lib/takeaway_kitchen.rb
@@ -1,16 +1,20 @@
 class TakeawayKitchen
   def initialize
     @menu = [
-      { dish: "dish1", price: 1 },
-      { dish: "dish2", price: 2 }
-      ]
+      { dish: "Edamame", price: 3 },
+      { dish: "Salmon Sashimi", price: 5 },
+      { dish: "Toro Sashimi", price: 7 },
+      { dish: "Salmon Nigiri", price: 6 },
+      { dish: "Toro Nigiri", price: 8 },
+      { dish: "California Maki", price: 7 },
+      { dish: "Matcha Mochi", price: 2 },
+      { dish: "Mango Mochi", price: 2 },
+    ]
+
   end
 
   def menu
     @menu.dup
   end
-
-  def return_menu
-
-  end
+  
 end
